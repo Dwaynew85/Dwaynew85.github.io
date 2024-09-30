4dd7bdd3a05e56e31fd861d9f45c390ad4448023
Dwaynew85 <mr_dwayne_w@yahoo.com>
2024-09-30 14:06:03 -0400

    floating nav and updated colors to match theme

--- components/ui/TextGenerateEffect.tsx
+++ components/ui/TextGenerateEffect.tsx
@@ -37,7 +37,7 @@ export const TextGenerateEffect = ({
           return (
             <motion.span
               key={word + idx}
-              className={`${idx > 3 ? 'text-purple' : 'dark:text-white text-black'} opacity-0`}
+              className={`${idx > 3 ? 'text-cyan' : 'dark:text-white text-black'} opacity-0`}
               style={{
                 filter: filter ? "blur(10px)" : "none",
               }}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -40,6 +40,7 @@ const config = {
           "100": "#E4ECFF",
         },
         purple: "#CBACF9",
+        cyan: "#00BFFF",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
