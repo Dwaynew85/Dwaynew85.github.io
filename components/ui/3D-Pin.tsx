dd721f1f0b395a256f4808735b118e7438280259
Dwaynew85 <mr_dwayne_w@yahoo.com>
2024-10-22 13:08:34 -0400

    3d pin container functionality improvements

--- components/ui/3D-Pin.tsx
+++ components/ui/3D-Pin.tsx
@@ -37,6 +37,8 @@ export const PinContainer = ({
       onMouseEnter={onMouseEnter}
       onMouseLeave={onMouseLeave}
       href={href || "/"}
+      target={"_blank"}
+      rel="noopener noreferrer"
     >
       <div
         style={{
@@ -54,25 +56,21 @@ export const PinContainer = ({
           <div className={cn(" relative z-50 ", className)}>{children}</div>
         </div>
       </div>
-      <PinPerspective title={title} href={href} />
+      <PinPerspective title={title} />
     </Link>
   );
 };
 
 export const PinPerspective = ({
   title,
-  href,
 }: {
   title?: string;
-  href?: string;
 }) => {
   return (
     <motion.div className="pointer-events-none  w-full h-80 flex items-center justify-center opacity-0 group-hover/pin:opacity-100 z-[60] transition duration-500">
       <div className=" w-full h-full -mt-7 flex-none  inset-0">
         <div className="absolute top-0 inset-x-0  flex justify-center">
-          <div
-            href={href}
-            target={"_blank"}
+          <div            
             className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 "
           >
             <span className="relative z-20 text-white text-xs font-bold inline-block py-0.5">
