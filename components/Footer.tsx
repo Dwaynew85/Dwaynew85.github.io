c911ce5ff871ee973ea2de71d5efefe8753392d3
Dwaynew85 <mr_dwayne_w@yahoo.com>
2024-11-01 12:07:39 -0400

    minor fixes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,13 +6,6 @@ import { socialMedia } from '@/data';
 const Footer = () => {
   return (
     <footer className='w-full pt-20 pb-10' id="contact">
-      <div className='w-full absolute left-0 -bottom-72 min-h-96'>
-        <img
-          src="/footer-grid.svg"
-          alt='grid'
-          className='w-full h-full opacity-75'
-        />
-      </div>
       <div className='flex flex-col items-center'>
         <h1 className='heading lg:max-w-[45vw]'>Ready to take <span className='text-cyan'>your</span> digital presence to the next level?</h1>
         <p className='text-white-200 md:mt-10 my-5 text-center'>Reach out to me today and let&apos;s discuss how I can help you achieve your goals!</p>
