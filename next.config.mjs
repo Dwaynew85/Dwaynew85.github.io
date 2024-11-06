29a858b48124b09d0fc1d3ad675f93f258d22cb6
Dwaynew85 <mr_dwayne_w@yahoo.com>
2024-11-05 22:29:02 -0500

    package updates and allow ssr

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true, 
   },
-  assetPrefix: isProd ? '/Dwaynew85.github.io/' : '',
-  basePath: isProd ? '/Dwaynew85.github.io' : '',
-  output: 'export',
 };
 
 export default nextConfig;
